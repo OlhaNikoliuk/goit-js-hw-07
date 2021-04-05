473d4ceccc1dd685143e48bda02208eec1d1f425
OlhaNikoliuk <nikoliukolha@gmail.com>
2021-04-05 12:46:00 +0300

    правки к задаче 1, решение(не все) задачи 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -13,3 +13,54 @@
  * Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
  * Создай функцию destroyBoxes(), которая очищает div#boxes.
  */
+
+const inputRef = document.querySelector('input');
+const createBtn = document.querySelector('[data-action="render"]');
+const destroyBtn = document.querySelector('[data-action="destroy"]');
+const boxesContainer = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', createBoxesContainer)
+destroyBtn.addEventListener('click', destroyBoxes);
+
+
+function randomColorMaker() {
+  const r = Math.floor(Math.random() * (256));
+  const g = Math.floor(Math.random() * (256));
+  const b = Math.floor(Math.random() * (256));
+  let randomColor = '#' + r.toString(16) + g.toString(16) + b.toString(16);
+  return randomColor;
+
+}
+
+function createBox() {
+  let newBox = document.createElement('div');
+  let newBoxSize = 30 + 'px'
+  newBox.style.width = newBoxSize;
+  newBox.style.height = newBoxSize;
+  newBox.style.backgroundColor = randomColorMaker()
+
+  return newBox;
+
+}
+
+function createBoxesContainer(amount) {
+  amount = inputRef.value;
+  let myDivs = [];
+  // newBoxSize += 10 + 'px';
+
+  // newBox.style.width = newBoxSize + '10px';
+  // newBox.style.height = newBoxSize + 10;
+  for (let i = 0; i <= amount - 1; i += 1) {
+
+    // newBoxSize += 10;
+    myDivs.push(createBox(){
+      newBoxSize = 10 * i + 'px'
+    })
+
+    boxesContainer.appendChild(myDivs[i]);
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
